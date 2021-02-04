efac2ef6ec55791cdd1f92c1559a126020bfa0c9
Christopher Chee <ketyung@techchee.com>
2021-02-04 09:04:43 +0800

    Initial Commit

---
+++ SwiftUIMVVMExample/SwiftUIMVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMExampleApp.swift
+//  SwiftUIMVVMExample
+//
+//  Created by Chee Ket Yung on 04/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
